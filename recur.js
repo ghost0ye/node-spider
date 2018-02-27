2c783b1bd9d05e592c7d9d8294163e6c49e29dc5
danjy <471049872@qq.com>
2018-02-27 16:23:58 +0800

    对爬虫的进一步优化，采用for循环调用接口。节省时间

--- recur.js
+++ recur.js
@@ -10,45 +10,65 @@ var opt = {
 };
 var originData = xlsx.parse(`${__dirname}/spider.xlsx`)[0].data;
 
+
+function segmentRecur(data, num = 0) {
+  let length = data.length;
+  let sublength = 10;
+  let end = Math.floor(length / sublength);
+  let rest = length % sublength;
+  if (num === end) {
+    for (let i = end * sublength; i < end * sublength + rest; i++) {
+      recur(data, i);
+    }
+    return;
+  } else {
+    for (let i = num * sublength; i < (num + 1) * sublength; i++) {
+      if (i === 0) {
+        data[0] = data[0].concat(['托管人', '投资类型', '募集规模（万元）', '是否结构化', '初始委托人数量']);
+      } else {
+        recur(data, i);
+      }
+    }
+  }
+  let timer = setInterval(function() {
+    let result = data.slice(num * sublength, (num + 1) * sublength);
+    if (result.every(el => el.length > 7)) {
+      clearInterval(timer);
+      writeFile(data, num, `segmengt ${num * 10} ~~ ${(num + 1) * 10} dnoe`);
+    }
+  }, 1000 / 60);
+  setTimeout(segmentRecur, 333, data, ++num);
+  // segmentRecur(data, ++num);
+}
+
+function recur(data, i) {
+  console.log(i);
+  request.post(url, { form: Object.assign(opt, { filter_EQS_MPI_ID: data[i][5] }) })
+    .then(res => {
+      res = JSON.parse(res)[0];
+      data[i] = data[i].concat([res.MPI_TRUSTEE, res.TZLX, res.MPI_TOTAL_MONEY, res.SFJGH, res.MPI_PARTICIPATION_USER]);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+
+
+
 if (fs.existsSync('log.txt')) {
   fs.readFile('log.txt', 'UTF-8', (err, data) => {
     if (err) throw new Error(err);
     let num = JSON.parse(data).num;
     if (originData.length) {
-      recur(originData, num);
+      segmentRecur(originData, num);
     }
   })
 } else {
-  fs.writeFile('log.txt', JSON.stringify({ num: 1 }), 'UTF-8', err => {
+  fs.writeFile('log.txt', JSON.stringify({ num: 0 }), 'UTF-8', err => {
     if (err) throw new Error(err);
   })
 }
-
-
-function recur(data, num) {
-  if (data[0].length < 7) {
-    data[0] = data[0].concat(['托管人', '投资类型', '募集规模（万元）', '是否结构化', '初始委托人数量']);
-  }
-  if (num > data.length - 1) {
-    writeFile(data, num, 'all done !!!');
-  } else {
-    if (num % 10 === 0) {
-      writeFile(data, num, `split 10 done~ num: ${num}`);
-    }
-    request.post(url, { form: Object.assign(opt, { filter_EQS_MPI_ID: data[num][5] }) })
-      .then(res => {
-        res = JSON.parse(res)[0];
-        data[num] = data[num].concat([res.MPI_TRUSTEE, res.TZLX, res.MPI_TOTAL_MONEY, res.SFJGH, res.MPI_PARTICIPATION_USER]);
-        num++;
-        console.log('request num: %s', num)
-        recur(data, num);
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
-}
-
 
 function writeFile(data, num, msg) {
   var buffer = xlsx.build([{ name: "spider", data }]); // Returns a buffer
